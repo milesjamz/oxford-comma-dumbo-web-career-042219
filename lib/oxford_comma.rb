2c792729c25ad200e5503c2f47f61e667d2d40c3
Miles Marillo <mjm03@hampshire.edu>
2019-03-31 20:47:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   return "#{array[0]}"
   elsif array.length == 2
   return array.join (" and ")
-  elsif array.length == 3
+  elsif array.length >= 3
   return array.join
   finalAnd = "and #{array[-1]}"
   array.pop
